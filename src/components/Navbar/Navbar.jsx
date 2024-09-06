a846f49d1b0a37d5e13c758aba2763b8b596a122
mpiech1813 <maciejpiech18@gmail.com>
2024-09-06 14:50:28 -0500

    added breakpoints

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 // use Link instead of <a> so the application can use cache
 
 import Links from "./link/links";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <div>Logo</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
       <Links />
     </div>
   );
