79ed611f35fbf1158a93360893dfcb332e919bb7
mpiech1813 <maciejpiech18@gmail.com>
2024-09-06 13:11:22 -0500

    created folders and files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 // import Navbar from '@/components/Navbar' => @ at can be used instead of multiple ../../../
 
 const Home = () => {
-  return <div>Hello World!</div>;
+  return <div>Homepage</div>;
 };
 
 export default Home;
