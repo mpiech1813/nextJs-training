907c6e64add0b9aa5769a5827f73caa25a5140e0
mpiech1813 <maciejpiech18@gmail.com>
2024-09-06 15:03:04 -0500

    day 1 finished

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="/about.jpg" alt="kol" fill />
+      </div>
+    </div>
+  );
 }
+
+// to add images from remote addresses, video: 1:05:48
+// next.config.js > images:{ remotePatterns: [{}]}
+// finishing today at 1:06:37
