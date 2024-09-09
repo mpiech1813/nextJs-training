af77b675fad95c7ac87e5bda802497fb4d84af2c
mpiech1813 <maciejpiech18@gmail.com>
2024-09-09 13:46:29 -0500

    contact page doen

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,3 +1,32 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="contact image"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="email" />
+          <input type="text" placeholder="phone number (optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
