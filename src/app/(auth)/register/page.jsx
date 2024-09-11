069b77e259d96111c6a8a70670d2cae79b978f3f
mpiech1813 <maciejpiech18@gmail.com>
2024-09-11 10:53:15 -0500

    register

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,3 +1,23 @@
+import { register } from "@/lib/actions";
+import styles from "./register.module.css";
+// import RegisterForm from
+
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  "use server";
+
+  return (
+    <div>
+      <form action=" ">
+        <input type="text" placeholder="username" name="username" />
+        <input type="text" placeholder="email" name="email" />
+        <input type="text" placeholder="password" name="password" />
+        <input
+          type="text"
+          placeholder="password repeat"
+          name="passwordRepeat"
+        />
+        <button>register</button>
+      </form>
+    </div>
+  );
 }
